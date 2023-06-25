6b43fe6df62b4c9cbc06ae1057636bbe169e082b
Justin-SG <schoenberg.justin@gmail.com>
2023-06-25 15:35:06 +0200

    Enabled CSR as well As the initial request should always be SSR, this should not be a problem The client would not hydrate unless CSR is enabled

--- src/routes/news/+page.server.ts
+++ src/routes/news/+page.server.ts
@@ -3,7 +3,7 @@ import prisma from '$lib/prismaClient';
 
 export const prerender = false;
 export const ssr = true;
-export const csr = false;
+export const csr = true;
 
 export const load = (async () => {
 	const posts = await prisma.post.findMany();
