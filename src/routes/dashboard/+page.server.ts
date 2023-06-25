3103662e793f483c74eeaa53296294409cd8abcc
Justin-SG <schoenberg.justin@gmail.com>
2023-06-25 15:24:38 +0200

    Enabled SSG

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -3,7 +3,7 @@ import prisma from '$lib/prismaClient';
 
 export const prerender = false;
 export const ssr = false;
-export const csr = true;
+export const csr = false;
 
 export const  load = (async () => {
 	const transactions = await prisma.transaction.findMany();
--- src/routes/news/+page.server.ts
+++ src/routes/news/+page.server.ts
@@ -1,9 +1,9 @@
 import type { PageServerLoad } from './$types';
 import prisma from '$lib/prismaClient';
 
-export const prerender = false;
+export const prerender = true;
 export const ssr = false;
-export const csr = true;
+export const csr = false;
 
 export const load = (async () => {
 	const posts = await prisma.post.findMany();
